6c64e6de477f1e5a3d9404561e61c6017d791f10
Rendi <artemrepin00@gmail.com>
2025-01-11 02:29:35 +0300

    HP bar

--- Assets/Scripts/Creatures/CreatureBase.cs
+++ Assets/Scripts/Creatures/CreatureBase.cs
@@ -1,29 +1,57 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CreatureBase : MonoBehaviour, IDamageable
 {
+    [SerializeField] protected float maxHealth = 100f;
     [SerializeField] protected float health = 100f;
     [SerializeField] protected ItemBase hand;
 
     [SerializeField] private GameObject damagePopUpPrefab;
 
+    private Slider healthSlider;
+
+    private void Awake()
+    {
+        healthSlider = GetComponentInChildren<Slider>();
+    }
+
     public virtual void Damage(float damage)
     {
         health -= damage;
-        GenerateDamagePopUp(damage);
+        GenerateDamagePopUp(damage, Color.red);
+        UpdateHealth();
 
         if (health <= 0)
             Destroy(gameObject);
     }
+    public virtual void Heal(float heal)
+    {
+        health += heal;
+        if (health > maxHealth)
+            health = maxHealth;
 
-    private void GenerateDamagePopUp(float damage)
+        GenerateDamagePopUp(heal, Color.green);
+        UpdateHealth();
+    }
+
+    private void GenerateDamagePopUp(float damage, Color color)
     {
         var popUp = Instantiate(damagePopUpPrefab, ParticleContainer.instance.transform);
         popUp.transform.position = transform.position + new Vector3(Random.Range(-.8f, .8f), 0.5f, -2);
         var text = popUp.GetComponentInChildren<TextMeshProUGUI>();
         text.text = damage.ToString();
+        text.color = color;
 
         Destroy(popUp, 1f);
+    }
+
+    private void UpdateHealth()
+    {
+        if (healthSlider != null)
+        {
+            healthSlider.value = health / maxHealth;
+        }
     }
 }
