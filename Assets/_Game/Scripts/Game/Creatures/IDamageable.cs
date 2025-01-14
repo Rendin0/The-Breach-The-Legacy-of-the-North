46ad5f2d5e5a03e68624bd0ec7dfabfae075df7c
Rendi <artemrepin00@gmail.com>
2025-01-14 07:31:17 +0300

    Deleted all old code

--- Assets/_Game/Scripts/Game/Creatures/IDamageable.cs
+++
@@ -1,5 +0,0 @@
-public interface IDamageable
-{
-    public void Damage(float damage);
-    public void Heal(float heal);
-}
--- Assets/_Game/Scripts/Game/Items/IUsable.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface IUsable
-{
-    public void Use(GameObject user, Vector2 direction);
-}
