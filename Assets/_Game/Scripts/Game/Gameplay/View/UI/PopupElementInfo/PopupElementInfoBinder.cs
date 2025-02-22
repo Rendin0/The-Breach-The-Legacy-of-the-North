5c8f7c5298752a0aa0c58cf861d398b2cffcb21a
Rendi <artemrepin00@gmail.com>
2025-02-22 04:54:58 +0300

    Position of element info

--- Assets/_Game/Scripts/Game/Gameplay/View/UI/PopupElementInfo/PopupElementInfoBinder.cs
+++ Assets/_Game/Scripts/Game/Gameplay/View/UI/PopupElementInfo/PopupElementInfoBinder.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 public class PopupElementInfoBinder : PopupBinder<PopupElementInfoViewModel>
 {
     [SerializeField] private TMP_Text _text;
+    [SerializeField] private RectTransform _container;
+    private readonly Vector3 _offset = new(0, 2);
 
     protected override void OnBind(PopupElementInfoViewModel viewModel)
     {
         base.OnBind(viewModel);
 
         _text.text = viewModel.Text;
+        _container.position = Input.mousePosition + _offset + new Vector3(0, _container.rect.size.y / 2);
+    }
+
+    private void Update()
+    {
+        _container.position = Input.mousePosition + _offset + new Vector3(0, _container.rect.size.y / 2);
     }
 }
