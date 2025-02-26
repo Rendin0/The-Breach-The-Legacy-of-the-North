b132456c6d56b23e8a6fda5c7bf7c01cab6ef56a
Rendi <artemrepin00@gmail.com>
2025-02-26 05:12:15 +0300

    Clear player prefs button in main menu

--- Assets/_Game/Scripts/Game/MainMenu/View/UI/ScreenMainMenu/ScreenMainMenuBinder.cs
+++ Assets/_Game/Scripts/Game/MainMenu/View/UI/ScreenMainMenu/ScreenMainMenuBinder.cs
@@ -6,12 +6,14 @@ public class ScreenMainMenuBinder : WindowBinder<ScreenMainMenuViewModel>
     [SerializeField] private Button _btnPlay;
     [SerializeField] private Button _btnSettings;
     [SerializeField] private Button _btnExit;
+    [SerializeField] private Button _btnClear;
 
     private void OnEnable()
     {
         _btnPlay.onClick.AddListener(OnPlayButtonClicked);
         _btnSettings.onClick.AddListener(OnSettingsButtonClicked);
         _btnExit.onClick.AddListener(OnExitButtonClicked);
+        _btnClear.onClick.AddListener(OnClearButtonClicked);
     }
 
     private void OnDisable()
@@ -19,6 +21,7 @@ public class ScreenMainMenuBinder : WindowBinder<ScreenMainMenuViewModel>
         _btnExit.onClick.RemoveAllListeners();
         _btnPlay.onClick.RemoveAllListeners();
         _btnSettings.onClick.RemoveAllListeners();
+        _btnClear.onClick.RemoveAllListeners();
     }
 
     private void OnExitButtonClicked()
@@ -34,5 +37,10 @@ public class ScreenMainMenuBinder : WindowBinder<ScreenMainMenuViewModel>
     private void OnPlayButtonClicked()
     {
         ViewModel.RequestPlay();
+    }
+
+    private void OnClearButtonClicked()
+    {
+        PlayerPrefs.DeleteAll();
     }
 }
