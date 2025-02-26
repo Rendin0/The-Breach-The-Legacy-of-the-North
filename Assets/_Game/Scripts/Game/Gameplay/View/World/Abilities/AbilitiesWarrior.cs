bd5bf5efcd305c1f991a7d54a7c8bab37c123626
Rendi <artemrepin00@gmail.com>
2025-02-26 05:36:51 +0300

    Misc

--- Assets/_Game/Scripts/Game/Gameplay/View/World/Abilities/AbilitiesWarrior.cs
+++ Assets/_Game/Scripts/Game/Gameplay/View/World/Abilities/AbilitiesWarrior.cs
@@ -47,10 +47,10 @@ public static class AbilitiesWarrior
 
         // Стан всех врагов в радиусе
         var hits = Physics2DUtils.GetCircleHits<CreatureBinder>(caster.Position.Value, stunRadius);
-        tmpEffect = new TemporaryStatusEffect(caster, new SEStun(), stunDuration);
 
         foreach (var hit in hits)
         {
+            tmpEffect = new TemporaryStatusEffect(hit.ViewModel, new SEStun(), stunDuration);
             hit.ViewModel.AddStatusEffect(tmpEffect);
         }
     }
@@ -81,8 +81,6 @@ public static class AbilitiesWarrior
     public static void EarthRift(CreatureViewModel caster, Vector2 mousePosition, Vector2 size, float debuffDuration, float damageAmplify)
     {
         Vector2 direction = (mousePosition - caster.Position.Value).normalized;
-        var stunEffect = new TemporaryStatusEffect(caster, new SEStun(), debuffDuration);
-        var amplifyEffect = new TemporaryStatusEffect(caster, new SEDefenseChange(damageAmplify, true), debuffDuration);
 
         var points = MathUtils.GetRectPoints(size, caster.Position.Value, direction);
         var hits = Physics2DUtils.GetColliderHits<CreatureBinder>(points);
@@ -93,6 +91,8 @@ public static class AbilitiesWarrior
         {
             if (hit.ViewModel.CreatureId == caster.CreatureId) continue;
 
+            var stunEffect = new TemporaryStatusEffect(hit.ViewModel, new SEStun(), debuffDuration);
+            var amplifyEffect = new TemporaryStatusEffect(hit.ViewModel, new SEDefenseChange(damageAmplify, true), debuffDuration);
             hit.ViewModel.AddStatusEffect(stunEffect);
             hit.ViewModel.AddStatusEffect(amplifyEffect);
         }
