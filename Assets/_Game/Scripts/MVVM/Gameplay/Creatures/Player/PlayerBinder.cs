5bb150eea24d3c690ea1521c4c28683736251f92
Rendi <artemrepin00@gmail.com>
2025-04-14 20:15:45 +0300

    Player animations

--- Assets/_Game/Scripts/MVVM/Gameplay/Creatures/Player/PlayerBinder.cs
+++ Assets/_Game/Scripts/MVVM/Gameplay/Creatures/Player/PlayerBinder.cs
@@ -1,14 +1,24 @@
 using R3;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerBinder : CreatureBinder
 {
     private Vector2 _direction;
+    private Vector2 _lastMoveDirection;
     private Camera _camera;
+
+    private Animator _animator;
+
     protected bool movementBlocked = false;
 
     private PlayerViewModel _playerViewModel;
     public override CreatureViewModel ViewModel => _playerViewModel;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     protected override void Start()
     {
@@ -27,8 +37,29 @@ public class PlayerBinder : CreatureBinder
     {
         base.FixedUpdate();
 
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        float moveMagnitude = rb.linearVelocity.magnitude;
+        
         if (!movementBlocked)
+        {
             rb.linearVelocity = ViewModel.Stats.Speed.Value * _direction;
+        }
+        _animator.SetFloat(Animator.StringToHash("MoveMagnitude"), moveMagnitude);
+
+        if (moveMagnitude > 0.1f)
+        {
+            _animator.SetFloat(Animator.StringToHash("MoveX"), rb.linearVelocityX);
+            _animator.SetFloat(Animator.StringToHash("MoveY"), rb.linearVelocityY);
+            _lastMoveDirection = _direction;
+            return;
+        }
+
+        _animator.SetFloat(Animator.StringToHash("LastMoveX"), _lastMoveDirection.x);
+        _animator.SetFloat(Animator.StringToHash("LastMoveY"), _lastMoveDirection.y);
     }
 
     protected override void OnBind(CreatureViewModel viewModel)
